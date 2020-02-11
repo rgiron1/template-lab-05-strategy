f80a3898a92e945376fec10ef516b8cfcf78295b
rgiron1 <rgiro004@hammer.cs.ucr.edu>
2020-02-11 10:55:50 -0800

    added Op class

---
+++ op.hpp
@@ -0,0 +1,13 @@
+#ifndef __OP_HPP__
+#define __OP_HPP__
+#include "base.hpp"
+
+class Op : public Base{
+public:
+	Op(double value) : Base() {val = value}
+	virtual double evaluate() { return val;}
+	virtual std::string stringify() {return std::to_string(val);}
+private:
+ double val;
+
+#endif
